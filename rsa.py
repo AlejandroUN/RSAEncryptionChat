d274c2ecacb1c993bb72a7179ae33f68b61b7efe
AlejandroUN <cchavezb@unal.edu.co>
2020-12-04 00:46:25 -0500

    Test

---
+++ rsa.py
@@ -0,0 +1,49 @@
+class rsa:
+
+    def findN(self, p, q):
+        return p*q
+
+    def findTotient(self, p, q):
+        return ((p-1)*(q-1))
+
+    def findTotientCoprime(self, totient):
+        for x in range(totient):
+            if self.findHCFWithEuclidsAlgorithm(x+1,totient) == 1:
+                return (x+1)
+        return 0
+
+    def findHCFWithEuclidsAlgorithm(self, a, b):
+        if a%b == 0:
+            return b
+        else:
+            return self.findHCFWithEuclidsAlgorithm(self, a, a%b)
+
+    def findInverse(self, e, totient):
+        for x in range(totient-1):
+            if ((e*(x+1))%totient) == 1:
+                return (x+1)
+        return 0
+
+    def codificateMessage(self, n, totientCoprime, message):
+        codedMessage = ""
+        for x in message:
+            currentModule = (ord(x)-96) % n
+            resultOfExpoModN = 1
+            for y in range(totientCoprime):
+                resultOfExpoModN = resultOfExpoModN*currentModule
+            codedMessage += str(resultOfExpoModN%n) + "/"
+        return codedMessage[:-1]
+
+    def decodeMessage(self, n, totientCoprimeInverseModTotient, codedMesage):
+        decodedMessage = ""
+        x = codedMesage.split("/")
+        for y in x:
+            resultOfExpoModN = 1
+            for z in range(totientCoprimeInverseModTotient):
+                resultOfExpoModN = resultOfExpoModN * int(y)
+            decodedMessage += str(chr((resultOfExpoModN % n)+96))
+        return decodedMessage
+gg = rsa()
+print(gg.codificateMessage(33,7,"holaz"))
+print(gg.decodeMessage(33,3,gg.codificateMessage(33,7,"holaz")))
+print(gg.findInverse(7,20))
---
+++ user.py
@@ -0,0 +1,30 @@
+from rsa import rsa
+
+
+class user:
+
+
+    def __init__(self, p, q):
+        self.p = p
+        self.q = q
+        self.n = rsa.findN(self,p,q)
+        self.totient = rsa.findTotient(self, p, q)
+        self.e = rsa.findTotientCoprime(self, self.totient)
+        self.d = rsa.findInverse(self, self.e, self.totient)
+
+    def sendMessage(self, message):
+        return rsa.codificateMessage(self, self.n, self.e, message)
+
+    def receiveMessage(self, message):
+        return rsa.decodeMessage(self, self.n, self.d, message)
+
+    def test(self):
+        return rsa.findN(self,3,11)
+
+    def getN(self):
+        return self.n
+
+gg = user(3,11)
+
+print(gg.sendMessage("hola"))
+print(gg.receiveMessage(gg.sendMessage("hola")))
