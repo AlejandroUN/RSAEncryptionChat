407eea4af0fa9b536fcf412647be1e6f2ee9e108
aeioon <stgstv585@gmail.com>
2020-12-05 09:38:21 -0500

    fast exponentiation, fixed some bugs and changed naming conventions

--- rsa.py
+++ rsa.py
@@ -53,21 +53,7 @@ class RSA:
         else:
             return RSA.gcd(b, a%b)
 
-    def egcd(a, b):
-        if a == 0:
-            return (b, 0, 1)
-        else:
-            g, y, x = RSA.egcd(b % a, a)
-            return (g, x - (b // a) * y, y)
-
-    def find_inverse(a, m):
-        g, x, y = RSA.egcd(a, m)
-        if g != 1:
-            raise Exception('modular inverse does not exist')
-        else:
-            return x % m
-
-    def find_inverse2(e, totient):
+    def find_inverse(e, totient):
         for x in range(totient-1):
             if ((e*(x+1))%totient) == 1:
                 return (x+1)
