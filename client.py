12d5d9410664430a75a8df3b74691ccec5e03baa
aeioon <stgstv585@gmail.com>
2020-12-11 23:02:14 -0500

    Modified sockets

--- client.py
+++ client.py
@@ -1,7 +1,105 @@
 import socket
+import select
+import errno
+import re
+import random
+import sys
+from rsa import RSA
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1234))
 
-msg = s.recv(1024)
-print(msg.decode("utf-8"))
+HEADER_LENGTH = 100
+
+IP = "127.0.0.1"
+PORT = 5555
+
+if(len(sys.argv) < 4):
+    print("{}-{}".format(sys.argv[1], sys.argv[2]))
+    rsa = RSA(int(sys.argv[1]), int(sys.argv[2]))
+else:
+    print("Ejecute los primos en la forma python client.py primo1 primo2")
+    exit()
+
+user_n, user_e = rsa.public_key
+user_n = user_n
+user_e = user_e
+
+public_key = "{}-{}".format(user_n, user_e)
+
+# Create a socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Connects to server
+client_socket.connect((IP, PORT))
+client_socket.setblocking(False)
+
+username = public_key.encode('utf-8')
+
+username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
+
+client_socket.send(username_header + username)
+
+other_user_pk = None
+other_user_n = None
+other_user_e = None
+
+while True:
+
+    message = input(f'{public_key} > ')
+
+    if message:
+
+        # Encode message to bytes
+        if(other_user_pk != None):
+            print("Enviando mensaje a usuario con pk", message, other_user_n, other_user_e)
+            message = rsa.codificate_message(other_user_n, other_user_e, message)
+            message = message.encode('utf-8')
+            message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
+            client_socket.send(message_header + message)
+        else: 
+            print("Esperando otro usuario. No se encodificara ningun mensaje")
+            message = '0'.encode('utf-8')
+            message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
+            client_socket.send(message_header + message)
+
+    try:
+
+        # We need to get the other person's public key. We do it in form of a username
+
+        while True:
+
+            username_header = client_socket.recv(HEADER_LENGTH)
+
+            # If we received no data, close connection
+            if not len(username_header):
+                print('Connection closed by the server')
+                sys.exit()
+
+            # Convert header to int value
+            username_length = int(username_header.decode('utf-8').strip())
+
+            # Receive and decode username
+            username = client_socket.recv(username_length).decode('utf-8')
+
+            # For now the other's user's pk is his username. So take it and encrypt messages with them
+            
+            other_user_pk = re.split("-", username)
+            other_user_n = int(other_user_pk[0])
+            other_user_e = int(other_user_pk[1])
+
+            message_header = client_socket.recv(HEADER_LENGTH)
+            message_length = int(message_header.decode('utf-8').strip())
+
+            message = client_socket.recv(message_length).decode('utf-8')
+            mensaje = rsa.decode_message(message)
+
+            print(f'{username} > {mensaje}')
+
+    except IOError as e:
+        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+            print('Reading error: {}'.format(str(e)))
+            sys.exit()
+
+        continue
+
+    except Exception as e:
+        print('Reading error: '.format(str(e)))
+        sys.exit()
--- server.py
+++ server.py
@@ -1,12 +1,82 @@
 import socket
+import select
+import re
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 1234))
-s.listen(5)
+HEADER_LENGTH = 100
+
+IP = "127.0.0.1"
+PORT = 5555
+FORMAT = 'utf-8'
+
+# Create a socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind((IP, PORT))
+
+server_socket.listen()
+
+sockets_list = [server_socket]
+
+# List of connected clients - socket as a key, user header and name as data
+clients = {}
+
+print(f'Listening on {IP}:{PORT}...')
+
+def receive_message(client_socket):
+    try:
+        message_header = client_socket.recv(HEADER_LENGTH)
+
+        if not len(message_header):
+            return False
+
+        message_length = int(message_header.decode(FORMAT).strip())
+
+        return {'header': message_header, 'data': client_socket.recv(message_length)}
+
+    except:
+        return False
+
 
 while True:
-    clientsocket, adress = s.accept()
-    print(f"Connections from {adress} has been established!")
-    clientsocket.send(bytes("Welcome to the server!", "utf-8"))
+    read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
 
+    for notified_socket in read_sockets:
+        if notified_socket == server_socket:
 
+            client_socket, client_address = server_socket.accept()
+
+            user = receive_message(client_socket)
+
+            if user is False:
+                continue
+
+            sockets_list.append(client_socket)
+
+            clients[client_socket] = user
+            print('New connection from {}:{}, username: {}'.format(*client_address, user['data'].decode(FORMAT)))
+
+        else:
+
+            message = receive_message(notified_socket)
+            
+            if message is False:
+                print('Closed connection from: {}'.format(clients[notified_socket]['data'].decode(FORMAT)))
+
+                sockets_list.remove(notified_socket)
+
+                del clients[notified_socket]
+
+                continue
+
+            user = clients[notified_socket]
+
+            print(f'Received message from {user["data"].decode(FORMAT)}: {message["data"].decode(FORMAT)}')
+
+            for client_socket in clients:
+                if client_socket != notified_socket:
+                    client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+
+    for notified_socket in exception_sockets:
+
+        sockets_list.remove(notified_socket)
+        del clients[notified_socket]
